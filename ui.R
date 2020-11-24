82eda90e40d95e7363453a405daebd32996f933f
ketanvaidya25 <72154792+ketanvaidya25@users.noreply.github.com>
2020-11-24 11:15:38 +0530

    Add files via upload
    Added background to shiny web app

--- ui.R
+++ ui.R
@@ -1,11 +1,21 @@
 data = read.csv('outcome-of-care-measures.csv')
 
 states = unique(data$State)
+if (interactive()){
 shinyServer(
-  pageWithSidebar(
-    headerPanel("Get the best ranking hospitals in US"),
+  fluidPage(
+    setBackgroundImage(
+      src = "https://cdn.hipwallpaper.com/i/15/72/wgMLUS.jpg"),
+    shinythemes::themeSelector(),
+    
+
+    headerPanel(strong("Get the best ranking hospitals in US")),
     
     sidebarPanel(
+      strong(p("Function Descriptions"),
+      p("1. Best Hospital - Input a state and the required medical condition, it will show you the best hospital for that respective condition in the state"),
+      p("2. rankHospital - Takes input the state, medical condition and a upper and lower limit of rank and displays the hospitals in the given rank range for the state"),
+      p("3. rankall - Takes input medical condition and a rank and displays hospitals at the given rank in all states")),
       selectInput("Task","Please select the task/function",
                   choices=c("Best Hospital",'rankHospital','rankall')),
       selectInput("Medical_Condition",'Specify the disease/medical condition',
@@ -28,7 +38,10 @@ shinyServer(
     mainPanel(
       textOutput("selected_var"),
       DT::dataTableOutput('sel2_var'),
-      DT::dataTableOutput('h_var')
+      DT::dataTableOutput('h_var'),
+      textOutput('besthosp')
+      
     )
   )
 )
+}
